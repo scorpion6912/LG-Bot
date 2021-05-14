c1c0bef91eff68f6d7f9d100340acb7d431a3f91
Léo barbier <leobarbier7@gmail.com>
2021-05-14 17:02:14 +0200

    ajout d'une ligne pour separer les blocs de code

--- mainbot.py
+++ mainbot.py
@@ -6,7 +6,8 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='!')
 load_dotenv(dotenv_path="../code bot/config")
 
-#login
+
+# login
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Streaming(name='regarde la nasa', url='https://www.twitch.tv/nasa'))
@@ -14,35 +15,41 @@ async def on_ready():
     print('{0.user}'.format(bot))
     print('------')
 
-#arriver membre (a tester)
+
+# arriver membre (a tester)
 @bot.event
 async def on_member_join(member):
     print(f"Un nouveau membre est arrivé : {member.display_name}")
 
-#spam juan (inutile)
+
+# spam juan (inutile)
 @bot.event
 async def test(ctx):
     if ctx == 290139158952017920:
         await ctx.send('sa va juan?'.format(ctx))
 
-#spam bot (inutile)
+
+# spam bot (inutile)
 @bot.event
 async def test(ctx):
     if ctx == 798950981609193532:
         await ctx.send('{0.author.mention} sa va le spam?'.format(ctx))
+
 
 # creation d'un channel textuel
 @bot.command(name="create")
 async def create(ctx):
     channel = await ctx.guild.create_text_channel('Loup-garrou')
 
-#mp une personne :eyes:
+
+# mp une personne :eyes:
 @bot.command(name="mp")
 async def download(message):
     channel = message.channel
     await message.author.send('👀')
 
-#commande pour del des messages dans un channel
+
+# commande pour del des messages dans un channel
 @bot.command(name="del")
 async def delete(ctx, number: int):
     messages = await ctx.channel.history(limit=number + 1).flatten()
@@ -50,29 +57,34 @@ async def delete(ctx, number: int):
     for each_message in messages:
         await each_message.delete()
 
-#bonjour c'est la moindre des choses
+
+# bonjour c'est la moindre des choses
 @bot.command(name='salut')
 async def salut(ctx):
     await ctx.channel.send('Salut {0.author.mention}'.format(ctx))
 
-#ping pong (a faire avec reponse des ms)
+
+# ping pong (a faire avec reponse des ms)
 @bot.command(name='ping')
 async def ping(ctx):
     await ctx.channel.send('pong'.format(ctx))
 
-#embed pour que sa soit plus jolie
+
+# embed pour que sa soit plus jolie
 @bot.command(name='oui')
 async def oui(ctx):
     embed = discord.Embed(title="foo", description="bar", color=0xFF0000)
     await ctx.send(embed=embed)
 
-#test connection dans channel vocal marche pas
+
+# test connection dans channel vocal marche pas
 @bot.command()
 async def join(ctx):
     channel = ctx.message.author.voice.channel
     voice = await channel.connect()
 
-#test deconnection dans channel vocal marche pas
+
+# test deconnection dans channel vocal marche pas
 @bot.command()
 async def leave(ctx):
     channel = ctx.message.author.voice.channel
