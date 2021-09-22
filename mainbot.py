4ab7dc6bbe7f8af3b3bfa8eb743922285881f59b
“Juan <juan.gomez-sanchez@etu.univ-lyon1.fr>
2021-09-22 09:43:01 +0200

    Correction des fautes d'orthographe

--- mainbot.py
+++ mainbot.py
@@ -9,7 +9,7 @@ bot = commands.Bot(command_prefix='!')
 load_dotenv(dotenv_path="../code bot/config")
 
 
-# login
+# Login
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name='!aled'))
@@ -18,14 +18,14 @@ async def on_ready():
     print('------')
 
 
-# creation d'un channel textuel
+# Création d'un salon textuel
 @bot.command(name="setup")
 async def setup(ctx):
     role = discord.utils.get(ctx.guild.roles, name="Villageois")
     if role is None:
         await ctx.guild.create_role(name="Villageois", colour=0xFF0F00, mentionable=True)
     else:
-        await ctx.send(f"Le rôle a deja été créer")
+        await ctx.send(f"Le rôle a déjà été créer")
     guild = ctx.guild
     channel_vocal = discord.utils.get(guild.channels, name='Village_vocal')
     if channel_vocal is None:
@@ -35,12 +35,12 @@ async def setup(ctx):
         role = discord.utils.get(ctx.guild.roles, name="Villageois")
         await channel_vocal.set_permissions(role, read_messages=True, send_messages=True)
     else:
-        await ctx.send(f"Le vocal a deja été créer")
+        await ctx.send(f"Le vocal a déjà été créer")
     channel = discord.utils.get(guild.text_channels, name='village')
     if channel is None:
         channel = await guild.create_text_channel('village')
         msg = await channel.send(
-            f"Les salons ont bien été créer merci de réagir avec : ➕ a ce messsage pour participer et ✅ pour lancer "
+            f"Les salons ont bien été créés, merci de réagir avec : ➕ à ce messsage pour participer puis ✅ pour lancer "
             f"la partie")
         await msg.add_reaction('➕')
         await msg.add_reaction('✅')
@@ -52,7 +52,8 @@ async def setup(ctx):
     else:
         await ctx.send(f"Les salons de jeux ont deja ete creer")
 
-
+# Création message après setup,à réagir pour savoir qui veut s'inscrire
+# S'inscrire
 @bot.event
 async def on_reaction_add(reaction, ctx):
     if ctx.id == bot.user.id:
@@ -63,8 +64,8 @@ async def on_reaction_add(reaction, ctx):
     if reaction.emoji == "➕":
         role = discord.utils.get(ctx.guild.roles, name='Villageois')
         msg = await channel.fetch_message(reaction.message.id)
-        if msg.content == (f"Les salons ont bien été créer merci de réagir avec : ➕ a ce messsage pour participer et ✅ pour lancer "
-        f"la partie"):
+        if msg.content == (f"Les salons ont bien été créés, merci de réagir avec : ➕ à ce messsage pour participer puis ✅ pour lancer "
+            f"la partie"):
             await ctx.add_roles(role)
             await channel.send("{0.mention} est inscrit".format(ctx))
         else:
@@ -73,8 +74,7 @@ async def on_reaction_add(reaction, ctx):
         msg = await channel.fetch_message(reaction.message.id)
         await msg.delete()
         await channel.set_permissions(ctx.guild.default_role, read_messages=False, send_messages=False)
-
-
+# Se désinscrire
 @bot.event
 async def on_raw_reaction_remove(payload):
     guild = bot.get_guild(payload.guild_id)
@@ -84,14 +84,14 @@ async def on_raw_reaction_remove(payload):
         return
     role = discord.utils.get(guild.roles, name='Villageois')
     msg = await channel.fetch_message(payload.message_id)
-    if msg.content == (f"Les salons ont bien été créer merci de réagir avec : ➕ a ce messsage pour participer et ✅ pour lancer "
-    f"la partie"):
+    if msg.content == (f"Les salons ont bien été créés, merci de réagir avec : ➕ à ce messsage pour participer puis ✅ pour lancer "
+        f"la partie"):
         await member.remove_roles(role)
         await channel.send(f"{member.mention} est désinscrit".format(member))
     else:
         print("autre channel remove")
 
-
+#Fonction de desetup
 @bot.command(name="desetup")
 async def desetup(ctx):
     guild = ctx.guild
@@ -104,9 +104,11 @@ async def desetup(ctx):
     channel = discord.utils.get(guild.text_channels, name='loup-garou')
     await channel.delete()
 
-
+#Affichage des commandes
 @bot.command(name="aled")
 async def aled(ctx):
+    embed = discord.Embed(title="foo", description="bar", color=0xFF0000)
+    await ctx.send(embed=embed)
     await ctx.author.send("voici les commandes que tu peux utiliser".format(ctx))
     await ctx.author.send("!setup permet de créer les channels ".format(ctx))
     await ctx.author.send("!desetup permet de supprimer les channels créer par le bot".format(ctx))
@@ -114,13 +116,13 @@ async def aled(ctx):
     await ctx.channel.send("va voir tes mp".format(ctx))
 
 
-# mp une personne :eyes:
+# Message privé une personne
 @bot.command(name="mp")
 async def download(message):
     await message.author.send('👀')
 
 
-# commande pour del des messages dans un channel
+# Commande pour effacer des messages dans un channel
 @bot.command(name="del")
 async def delete(ctx, number: int):
     messages = await ctx.channel.history(limit=number + 1).flatten()
@@ -129,33 +131,33 @@ async def delete(ctx, number: int):
         await each_message.delete()
 
 
-# bonjour c'est la moindre des choses
-@bot.command(name='salut')
-async def salut(ctx):
-    await ctx.channel.send('Salut {0.author.mention}'.format(ctx))
+# Commande bonjour
+@bot.command(name='bonjour')
+async def bonjour(ctx):
+    await ctx.channel.send('Bonjour {0.author.mention} !'.format(ctx))
 
 
-# ping pong (a faire avec reponse des ms)
+# Ping (pong)
 @bot.command(name='ping')
 async def ping(ctx):
     await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
 
 
-# embed pour que sa soit plus jolie
+# Embed pour que ce soit plus joli
 @bot.command(name='oui')
 async def oui(ctx):
     embed = discord.Embed(title="foo", description="bar", color=0xFF0000)
     await ctx.send(embed=embed)
 
 
-# connection dans channel vocal
+# Connexion dans salon vocal
 @bot.command()
 async def join(ctx):
     channel = ctx.author.voice.channel
     await channel.connect()
 
 
-# deconnection dans channel vocal
+# Déconnexion dans salon vocal
 @bot.command()
 async def leave(ctx):
     await ctx.voice_client.disconnect()
